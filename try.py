5017a2d149b13669ac3f5675ba3839a03efccc36
De-Morgan7 <niiablorhde60@gmail.com>
2025-09-15 16:15:26 +0000

    Add files via upload
    assignment 2

---
+++ try.py
@@ -0,0 +1,15 @@
+
+
+mylist= []
+mylist.append(10)
+mylist.append(20)
+mylist.append(30)   
+mylist.append(40)
+mylist.insert(1,15)
+mylist.extend([50,60,70])
+mylist.pop()
+mylist.sort()
+print("final list: ",mylist)
+indexof30 = mylist.index(30)
+print("index of 30: ",indexof30)
+  
